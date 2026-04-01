b1bc79c03726b0f896a031e41452d612ad43df3f
Siddh77 <siddharth.kathiravan07@gmail.com>
2026-04-01 15:14:23 +0530

    Siddharth Added RoomInitialisation

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,6 +1,25 @@
 public class BookMyStayApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        SingleRoom single = new SingleRoom();
+        DoubleRoom doubleRoom = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails(singleAvailable);
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails(doubleAvailable);
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails(suiteAvailable);
     }
+
 }
